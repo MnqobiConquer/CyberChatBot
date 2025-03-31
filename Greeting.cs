cd18e23780fa94b37e895c5d2f993af995646f8f
Mnqobi.Ntereke <Mnqobi.Ntereke@DDNADD5R6HV2>
2025-03-31 10:17:16 +0200

    Add project files.

---
+++ Greeting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CyberChatBot
+{
+    internal class Greeting 
+    {
+        //public static void PlayVoiceGreeting()
+        //{
+        //    try
+        //    {
+        //        SoundPlayer player = new SoundPlayer("greeting.wav");
+        //        player.PlaySync(); // Play audio synchronously
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Error playing audio: " + e.Message);
+        //    }
+        //}
+        public static void AskUserName()
+        {
+
+            AnimatedText("Hi there I'm Pupet, What's your name & Welcome to the Cyber Security Awareness Bot!", 30);
+            Console.ResetColor();
+            string name = Console.ReadLine(); 
+            if (string.IsNullOrWhiteSpace(name)) 
+            {
+                name = "Friend"; 
+            }
+
+            AnimatedText($"\nHello, {name}! Let's learn about cyber security.\n", 30);
+            Thread.Sleep(500);
+        }
+        static void AnimatedText(string text, int delay = 40)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
+            Console.WriteLine();
+        }
+    }
+}
